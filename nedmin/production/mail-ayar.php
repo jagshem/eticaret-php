94a27af89368cefcfd97e335cc6cb4de0ae43f40
Tunahan Dogan <piyealar@gmail.com>
2023-09-28 18:52:04 +0300

    Admin panel değişiklikler

--- nedmin/production/mail-ayar.php
+++ nedmin/production/mail-ayar.php
@@ -8,7 +8,7 @@
             <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                     <div class="x_title">
-                        <h2>Api Ayarlar <small>İşlem Durumu <?php if ($_GET['durum'] == "ok") { ?>
+                        <h2>Api Ayarları <small>İşlem Durumu <?php if ($_GET['durum'] == "ok") { ?>
                                     <br>
                                     <br>
                                     <strong class="green">Ayarlar Başarıyla Güncellendi!</strong>
@@ -38,9 +38,9 @@
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
                                 <ul class="dropdown-menu" role="menu">
-                                    <li><a href="#">Settings 1</a>
+                                    <li><a href="#">Ayarlar 1</a>
                                     </li>
-                                    <li><a href="#">Settings 2</a>
+                                    <li><a href="#">Ayarlar 2</a>
                                     </li>
                                 </ul>
                             </li>
